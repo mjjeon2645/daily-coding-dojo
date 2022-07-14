cccd7bb46553fd019c6b00ada5e482838e07c3af
Jeon Minji <mjjeon2645@gmail.com>
2022-07-14 09:51:57 +0900

    List<Integer>로 자료구조를 변형하였더니 효율성검사 통과

--- 220714-dojo/app/src/main/java/Solution.java
+++ 220714-dojo/app/src/main/java/Solution.java
@@ -4,20 +4,20 @@ public class Solution {
   public int[] solution(int []arr) {
     int[] answer = {};
 
-    String accumulator = "";
+    List<Integer> accumulator = new ArrayList<>();
 
-    accumulator += arr[0];
+    accumulator.add(arr[0]);
 
     for (int i = 1; i < arr.length; i += 1) {
       if (arr[i - 1] != (arr[i])) {
-        accumulator += arr[i];
+        accumulator.add(arr[i]);
       }
     }
 
-    answer = new int[accumulator.length()];
+    answer = new int[accumulator.size()];
 
-    for (int i = 0; i < accumulator.length(); i += 1) {
-      answer[i] = Character.getNumericValue(accumulator.charAt(i));
+    for (int i = 0; i < accumulator.size(); i += 1) {
+      answer[i] = accumulator.get(i);
     }
 
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
