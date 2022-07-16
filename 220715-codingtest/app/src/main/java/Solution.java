869da185aee44ca2cc35e5b02841c019c850fa9a
Jeon Minji <mjjeon2645@gmail.com>
2022-07-16 11:20:05 +0900

    동료 코드 참고하여 해결 완료
    궁금한 점
    -> 14번 라인에 java가 자동으로 바꿔준 식이 있는데 해당 내용은 프로그래머스에서 통과되지 않음
    boxed().이후로 collect(Collectors.toList()) 이렇게 모두 기재해야 통과되는데 이유를 잘 모르겠음

--- 220715-codingtest/app/src/main/java/Solution.java
+++ 220715-codingtest/app/src/main/java/Solution.java
@@ -1,61 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
   public int[] solution(int N, int[] stages) {
     int[] answer = new int[N];
-    float[] failRate = new float[N];
-
     int people = stages.length;
-    int accumulator = 0;
+    int failPeople = 0;
+    double failRate= 0;
 
-    for(int i = 0; i < N; i += 1) {
-      for (int j = 0; j < stages.length; j += 1) {
-        if (stages[j] == (i + 1)) {
-          accumulator += 1;
-        }
-        failRate[i] = accumulator / people;
-        people -= accumulator;
-        accumulator = 0;
+    Map<Integer, Double> failRates = new HashMap<>();
 
-        if (people == 0) {
-          people = 1;
-          // TODO. 남은 배열이 있다면 그 자리에 모두 0을 집어넣고 탈출
-        }
+    List<Integer> stageLists = Arrays.stream(stages).boxed().toList();
+
+    for (int i = 1; i <= N; i += 1) {
+      failPeople = Collections.frequency(stageLists, i);
+
+      if (failPeople != 0) {
+        failRate = (double) failPeople / people;
+        people -= failPeople;
       }
+
+      if (failPeople == 0) {
+        failRate = 0;
+      }
+
+      failRates.put(i, failRate);
     }
 
+    List<Integer> keyLists = new ArrayList<>(failRates.keySet());
 
+    Collections.sort(keyLists, (val1, val2) -> failRates.get(val2).compareTo(failRates.get(val1)));
 
+    int i = 0;
+    for (Integer key : keyLists) {
+      answer[i] = key;
+    i += 1;
+    }
 
     return answer;
   }
 }
-
-
-
-
-
-//    List<Integer> stagesLists = new ArrayList<>();
-//
-//    for (int stage : stages) {
-//      stagesLists.add(stage);
-//    }
-//
-//    int people = stages.length;
-//
-//    for (int i = 0; i < N; i += 1) {
-//      int finalI = i;
-//      int number = (int) stagesLists.stream().filter(num -> num == (finalI + 1)).count();
-//
-//      failRate[i] = number / people;
-//
-//      people -= number;
-//    }
-//
-//
-//
-//
-//    for (int i = 1; i <= N + 1; i += 1) {
-//
-//    }
