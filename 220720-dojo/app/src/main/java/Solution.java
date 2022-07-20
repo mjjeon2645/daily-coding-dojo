146773d05bd401f0e7d4a691a82d7e073ef51798
Jeon Minji <mjjeon2645@gmail.com>
2022-07-20 22:36:55 +0900

    동료 코드 참고하여 코딩도장 문제 해결
    1. 이게... 이렇게 쉬운 문제를 왜 이렇게 어렵게 생각했을까. 사실 최소공약수 구하는 건 너무나 간단하고도 쉬웠는데 최소공배수에서 막혔음

--- 220720-dojo/app/src/main/java/Solution.java
+++ 220720-dojo/app/src/main/java/Solution.java
@@ -1,66 +1,20 @@
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Solution {
   public int[] solution(int n, int m) {
     int[] answer = new int[2];
 
-    long smallNumber = 0;
-    long largeNumber = 0;
-
-    if (n > m) {
-      smallNumber = m;
-      largeNumber = n;
-    }
-
-    if (n < m) {
-      smallNumber = n;
-      largeNumber = m;
-    }
-
-    if (n == m) {
+    if (m % n == 0) {
       answer[0] = n;
-      answer[1] = n;
-      return answer;
+      answer[1] = m;
     }
 
-    if (largeNumber % smallNumber == 0) {
-      answer[0] = (int) smallNumber;
-      answer[1] = (int) largeNumber;
-      return answer;
-    }
-
-    String number = "";
-    long dividend = largeNumber;
-
-    for (int i = 2; i <= largeNumber; i += 1) {
-      if (dividend % i == 0) {
-        number += i;
-        dividend /= i;
+    if (m % n != 0) {
+      for (int i = 1; i <= m; i++) {
+        if (n % i == 0 && m % i == 0) {
+          answer[0] = i;
+        }
       }
+      answer[1] = n * m / answer[0];
     }
-
-    String[] numbers1 = number.split("");
-
-    number = "";
-    dividend = smallNumber;
-
-    for (int i = 2; i <= smallNumber; i += 1) {
-      if (dividend % i == 0) {
-        number += i;
-        dividend /= i;
-      }
-    }
-
-    String[] numbers2 = number.split("");
-
-    Arrays.sort(numbers1);
-    Arrays.sort(numbers2);
-
-
-
-    return new int[]{0, 0};
+    return answer;
   }
 }
