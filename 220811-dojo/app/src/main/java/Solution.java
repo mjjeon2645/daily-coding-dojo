d58f533b3613a8bcb6b85065d6c8f68cffa3bbb7
Jeon Minji <mjjeon2645@gmail.com>
2022-08-11 09:25:00 +0900

    나머지를 구하는 함수인 Math.floorMod(피제수, 제수) 활용해 봄
    거의 안써본 함수인 것 같은데 찾아서 사용해 봄

--- 220811-dojo/app/src/main/java/Solution.java
+++ 220811-dojo/app/src/main/java/Solution.java
@@ -1,18 +1,15 @@
 public class Solution {
   public int solution(int n) {
     int answer = 0;
-
     int i = 1;
 
-    while (i <= n - 1) {
-      if (n % i == 1) {
+    while(i <= n) {
+      if (Math.floorMod(n, i) == 1) {
         answer = i;
         break;
       }
-
       i += 1;
     }
-
     return answer;
   }
 }
