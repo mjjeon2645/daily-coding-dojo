f47f3345fbdeeb10a4003711cfc46cb17d73e280
Jeon Minji <mjjeon2645@gmail.com>
2022-08-14 19:47:03 +0900

    동료 코드 참고하여 기한 내 제출
    꼭 해결하고만다...부들부들..

--- 220812-dojo/app/src/main/java/Solution.java
+++ 220812-dojo/app/src/main/java/Solution.java
@@ -1,69 +1,26 @@
 class Solution {
   public String[] solution(int n, int[] arr1, int[] arr2) {
-    String[] answer = {};
-    arr1 = new int[n];
-    String[] newArray1 = new String[n];
-
-    for (int i = 0; i < n; i += 1) {
-      int dividend = arr1[i];
-      String number = "";
-
-      while (dividend > 0) {
-        number = dividend % 2 + number;
-        dividend /= 2;
+    String[] answer = new String[n];
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      count = 0;
+      answer[i] = (Integer.toBinaryString(arr1[i] | arr2[i]));
+      if (answer[i].length() != n) {
+        count = n - answer[i].length();
       }
 
-      if (number.length() == 5) {
-        newArray1[i] = number;
-      }
-
-      if (number.length() != 5) {
-        while (number.length() < 5) {
-          number = "0" + number;
-        }
-        newArray1[i] = number;
+      for (int j = 0; j < count; j++) {
+        answer[i] = "0" + answer[i];
       }
     }
 
-    arr2 = new int[n];
-    String[] newArray2 = new String[n];
-
-    for (int i = 0; i < n; i += 1) {
-      int dividend = arr2[i];
-      String number = "";
-
-      while (dividend > 0) {
-        number = dividend % 2 + number;
-        dividend /= 2;
-      }
-
-      if (number.length() == 5) {
-        newArray2[i] = number;
-      }
-
-      if (number.length() != 5) {
-        while (number.length() < 5) {
-          number = "0" + number;
-        }
-        newArray2[i] = number;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        answer[i] = answer[i].replace("1", "#");
+        answer[i] = answer[i].replace("0", " ");
       }
     }
 
-    for (int i = 0; i < n; i += 1) {
-      String answerString = "";
-
-      for (int j = 0; j < n; j += 1) {
-        answerString = "";
-        char a = newArray1[i].charAt(j);
-        char b = newArray2[i].charAt(j);
-
-        if (a == b && b == '0') {
-          answerString += " ";
-        }
-        answerString += "#";
-      }
-      answer[i] = answerString;
-    }
     return answer;
   }
 }
