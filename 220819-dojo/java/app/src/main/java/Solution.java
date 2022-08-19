d2a5ac54554e10a528fcfc5502d7284b472beaf8
Jeon Minji <mjjeon2645@gmail.com>
2022-08-19 16:36:33 +0900

    220819 코딩도장 시저암호-자바문제 해결
    중복 코드가 있어서 해결해주고 싶은데 방법을 아직 잘 모르겠음

--- 220819-dojo/java/app/src/main/java/Solution.java
+++ 220819-dojo/java/app/src/main/java/Solution.java
@@ -5,7 +5,6 @@ public class Solution {
   public String solution(String s, int n) {
     String answer = "";
     String alphabetCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; // 0~25, 26~51
-//    String lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
     String[] words = s.split("");
 
@@ -17,15 +16,15 @@ public class Solution {
       int index = alphabetCase.indexOf(words[i]);
 
       if (index >= 0 && index <= 25) {
-        words[i] = alphabetCase.charAt((index + n) % 25) + "";
+        words[i] = index + n <= 25
+            ? alphabetCase.charAt(index + n) + ""
+            : alphabetCase.charAt(index + n - 26) + "";
       }
 
-      if (index >= 26 && index <= 51) {
-        if (index + n <= 51) {
-          words[i] = alphabetCase.charAt(index + n)+ "";
-        }
-
-        words[i] = alphabetCase.charAt(index + n - 26) + "";
+      if (index >= 26) {
+        words[i] = index + n <= 51
+            ? alphabetCase.charAt(index + n) + ""
+            : alphabetCase.charAt(index + n - 26) + "";
       }
     }
 
