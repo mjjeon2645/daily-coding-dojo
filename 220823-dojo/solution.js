488479ac098eb84de5ba6af92c4c40825c93b08d
Jeon Minji <mjjeon2645@gmail.com>
2022-08-23 10:38:59 +0900

    임시 커밋

---
+++ 220823-dojo/solution.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/prefer-default-export
+export function solution(array, commands) {
+//   const answer = [];
+
+  const numberForTest = array.slice(commands[0][0] - 1, commands[0][1])
+    .sort().;
+
+  return numberForTest;
+}
