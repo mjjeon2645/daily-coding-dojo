814c5ecc395f36f8bf2a6b25913a487783b4fc47
Jeon Minji <mjjeon2645@gmail.com>
2022-08-24 01:12:01 +0900

    자바스크립트로 해결

--- 220823-dojo/solution.js
+++ 220823-dojo/solution.js
@@ -1,9 +1,15 @@
+/* eslint-disable no-console */
 // eslint-disable-next-line import/prefer-default-export
 export function solution(array, commands) {
-//   const answer = [];
+  const answer = [];
 
-  const numberForTest = array.slice(commands[0][0] - 1, commands[0][1])
-    .sort().;
+  for (let i = 0; i < commands.length; i += 1) {
+    const slicedArray = array.slice(commands[i][0] - 1, commands[i][1]);
 
-  return numberForTest;
+    const sortedArray = slicedArray.sort((a, b) => a - b);
+
+    answer.push(sortedArray[commands[i][2] - 1]);
+  }
+
+  return answer;
 }
--- 220823-dojo/solution.test.js
+++ 220823-dojo/solution.test.js
@@ -2,5 +2,5 @@ import { solution } from './solution';
 
 test('numberForTest', () => {
   expect(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
-    .toBe(5);
+    .toStrictEqual([5, 6, 3]);
 });
