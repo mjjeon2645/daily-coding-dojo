4f08aed6208b579faf339dfaa795ab66132321d2
Jeon Minji <mjjeon2645@gmail.com>
2022-08-30 09:48:42 +0900

    220830 코딩도장-완주하지 못한 선수
    자바스크립트로 풀어봄.
    for문을 최대한 쓰지 않으면서 map을 활용하는 방법에 대해 알고싶다..
    오늘도 for문의 굴레에서 벗어나지 못했는데 자바스크립트 언어 자체에 좀 익숙해질 필요가 있다.
    처음에 어떻게 해야 할지 몰라서 다른 코드들을 좀 참고했고 그렇게 물꼬를 트고 나니
    속도감있게 풀 수 있었음.

---
+++ 220830-dojo/solution.test.js
@@ -0,0 +1,26 @@
+import { sortParticipantArray, sortCompletionArray, solution } from './solution';
+
+test('sortingParticipantArray', () => {
+  expect(sortParticipantArray(['a', 'ab', 'ab'], ['b', 'a']))
+    .toStrictEqual(['a', 'ab', 'ab'], ['a', 'b']);
+});
+
+test('sortingCompletionARray', () => {
+  expect(sortCompletionArray(['a', 'z', 'd'], ['b', 'a']))
+    .toStrictEqual(['a', 'd', 'z'], ['a', 'b']);
+});
+
+test('solution', () => {
+  expect(solution(['leo', 'kiki', 'eden'], ['eden', 'kiki']))
+    .toStrictEqual(['leo']);
+  expect(solution(
+    ['marina', 'josipa', 'nikola', 'vinko', 'filipa'],
+    ['josipa', 'filipa', 'marina', 'nikola'],
+  ))
+    .toStrictEqual(['vinko']);
+  expect(solution(
+    ['mislav', 'stanko', 'mislav', 'ana'],
+    ['stanko', 'ana', 'mislav'],
+  ))
+    .toStrictEqual(['mislav']);
+});
