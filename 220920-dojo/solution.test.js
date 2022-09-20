cfaa768c9dc31b950047d86c4cdc7a3bc6cb08f2
Jeon Minji <mjjeon2645@gmail.com>
2022-09-20 10:16:45 +0900

    220920-코딩도장 n-queen
    손도 못댔다. 후~

---
+++ 220920-dojo/solution.test.js
@@ -0,0 +1,11 @@
+import solution, { check } from './solution';
+
+test('Solution', () => {
+  expect(solution(1)).toBe(1);
+  expect(solution(2)).toBe(0);
+});
+
+test('check', () => {
+  expect(solution(1)).toBe(1);
+  expect(solution(2)).toBe(0);
+});
