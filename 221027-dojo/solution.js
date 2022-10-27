782a9c0e582ea9e0077dbfb0ad98a34b603f31b4
Jeon Minji <mjjeon2645@gmail.com>
2022-10-27 20:01:01 +0900

    221027-코딩도장-숫자의 표현(자바스크립트)

--- 221027-dojo/solution.js
+++ 221027-dojo/solution.js
@@ -1,53 +1,11 @@
-export function makeArray(number) {
-  return Array.from({ length: number }, (value, index) => index + 1);
-}
-
-export function accumulator(array, number) {
-  if (array.length === 1) {
-    return -2;
-  }
-
-  return array.reduce(
-    (acc, cur) => {
-      if (accumulator === number) {
-        return 0;
-      }
-
-      if (accumulator > number) {
-        return -1;
-      }
-
-      return acc + cur;
-    }
-    , 0,
-  );
-}
-
-export function process(arrayFromNumber, number) {
-  accumulator(arrayFromNumber, number);
-}
-
 export default function solution(n) {
-  const answer = 4;
+  const answer = [];
 
-  const arrayFromNumber = makeArray(n);
-
-  const count = [];
-
-  const result = accumulator(arrayFromNumber, n);
-
-  if (result === -1) {
-    accumulator(arrayFromNumber.shift(), n);
+  for (let i = 1; i <= n; i += 1) {
+    if (n % i === 0 && i % 2 === 1) {
+      answer.push(1);
+    }
   }
 
-  if (result === 0) {
-    count.push(1);
-    accumulator(arrayFromNumber.shift(), n);
-  }
-
-  if (result === -2) {
-    return count.length;
-  }
-
-  return answer;
+  return answer.length;
 }
--- 221027-dojo/solution.test.js
+++ 221027-dojo/solution.test.js
@@ -1,13 +1,5 @@
-import solution, { makeArray, accumulator } from './solution';
+import solution from './solution';
 
 test('solution', () => {
   expect(solution(15)).toBe(4);
-});
-
-test('makeArray', () => {
-  expect(makeArray(3)).toStrictEqual([1, 2, 3]);
-});
-
-test('accumulator', () => {
-  expect(accumulator([1, 2, 3])).toBe(6);
 });
