0e660381de3e8a7df58c85263e77d5e1279c6523
Jeon Minji <mjjeon2645@gmail.com>
2022-11-28 09:01:31 +0900

    221128-코딩도장-타겟넘버-자바

--- 221125-dojo/java/app/src/main/java/Solution.java
+++ 221125-dojo/java/app/src/main/java/Solution.java
@@ -1,12 +1,16 @@
 class Solution {
-    public int solution(int[] numbers, int target) {
-        int answer = 0;
-
-        if (numbers.length == 2) {
-            if(numbers[0] == numbers[1] && numbers[0] + numbers[1] == target) {
-                return 1;
-            }
+    int answer = 0;
+    public void dfs(int[] num, int total, int depth, int target) {
+        if (depth == 0) {
+            if (total == target)
+                answer++;
+            return;
         }
+        dfs(num, total + num[num.length - depth], depth - 1, target);
+        dfs(num, total - num[num.length - depth], depth - 1, target);
+    }
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, numbers.length, target);
         return answer;
     }
 }
